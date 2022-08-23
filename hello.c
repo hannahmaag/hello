ff7ab51cf882ee618f0963a543c69bd0e0be745e
Hannah Maag <hmaag2@huskers.unl.edu>
2022-08-23 22:34:48 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Hannah Maag
+ *         hannahmaag@gmail.com
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hannah Maag\n");
+
+  return 0;
+}
